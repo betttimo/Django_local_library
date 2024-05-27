8ec7b0b34fb07fee3c1bd979159450b8d097a24e
betttimo <timothykipngetich0@gmail.com>
2024-05-27 14:59:06 +0300

    Changes not covered

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -10,7 +10,3 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
